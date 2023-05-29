5e15996c4ff3a4860e924bdc8d398c6d50cd364f
gizha <gizha@LAYZ1HAG>
2023-05-29 03:42:37 +0300

    Добавьте файлы проекта.

---
+++ 1 zadanie/Pages/people.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _1_zadanie.Pages
+{
+    public class peopleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
